e41ac581be7926fb482d31ba2c14f9aa84189d3a
samara-c <41492804+samara-c@users.noreply.github.com>
2020-06-09 13:26:29 -0300

    Add files via upload

--- validador.js
+++ validador.js
@@ -1,6 +1,4 @@
-<script type = "text/javascript">
-   <!--
-      // Form validation code will come here.
+
       function validate() {
         var x = document.getElementById("formNome").value;
          if( x == "" ) {
@@ -8,23 +6,4 @@
             document.formPrincipal.formularioNome.focus() ;
             return false;
          }
-         // if( document.myForm.EMail.value == "" ) {
-         //    alert( "Please provide your Email!" );
-         //    document.myForm.EMail.focus() ;
-         //    return false;
-         // }
-         // if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
-         //    document.myForm.Zip.value.length != 5 ) {
-         //
-         //    alert( "Please provide a zip in the format #####." );
-         //    document.myForm.Zip.focus() ;
-         //    return false;
-         // }
-         // if( document.myForm.Country.value == "-1" ) {
-         //    alert( "Please provide your country!" );
-         //    return false;
-         }
-         return( true );
        }
-   //-->
-</script>
